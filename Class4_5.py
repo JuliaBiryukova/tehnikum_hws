1a4a0fa053074f0c1ded0ae6e0ddefb63bf57f15
YenotKuzya <75937913+YenotKuzya@users.noreply.github.com>
2020-12-18 23:40:31 +0500

    Update Class4_5.py

--- Class4_5.py
+++ Class4_5.py
@@ -1,4 +1,9 @@
-print('Enter two words <3')
-m = input()
-m = m.split(' ')
-print(m[1], m[0])
+while True:
+    print('Enter two words <3')
+    m = input()
+    m = m.split(' ')
+    print(m[1], m[0])
+    print('Write "0" if you wanr to stop')
+    n = input()
+    if n == '0':
+        break
