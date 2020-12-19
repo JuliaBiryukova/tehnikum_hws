090b8a490c461e68728f617a4bb7eb88c8e7ebcc
YenotKuzya <75937913+YenotKuzya@users.noreply.github.com>
2020-12-19 08:42:27 +0500

    Update and rename Class4_5.py to Class4/Zadaniye5.py

--- Class4/Zadaniye5.py
+++ Class4/Zadaniye5.py
@@ -3,7 +3,7 @@ while True:
     m = input()
     m = m.split(' ')
     print(m[1], m[0])
-    print('Write "0" if you wanr to stop')
+    print('Write "0" to stop')
     n = input()
     if n == '0':
         break
