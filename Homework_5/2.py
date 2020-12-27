639ef99166812760922daf42dd136bd0c80fd3a5
YenotKuzya <75937913+YenotKuzya@users.noreply.github.com>
2020-12-27 19:06:58 +0500

    Update 2.py

--- Homework_5/2.py
+++ Homework_5/2.py
@@ -6,14 +6,14 @@ def start(update, context):
     context.bot.send_message(chat_id=update.message.chat_id,
                              text='Хеллоооу!' + name)
 
-def adduser(update, context):
+def add(update, context):
     l = open('text.txt', 'a')
     name = update.message.from_user.first_name
     l.write(name + '\n')
     context.bot.add_user(chat_id=update.message.chat_id, text='Мы добавили вас в список')
 
 
-def deluser(update, context):
+def delite(update, context):
     name = update.message.from_user.first_name
     n = []
     l = open('text.txt', 'r')
@@ -38,8 +38,8 @@ TOKEN = '1484306825:AAEUjpOuGHe7dAqGvT166U3jppMPCL2e60k'
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('adduser', adduser))
-dispatcher.add_handler(CommandHandler('deluser', deluser))
+dispatcher.add_handler(CommandHandler('adduser', add))
+dispatcher.add_handler(CommandHandler('deluser', delite))
 dispatcher.add_handler(CommandHandler('bye', bye))
 
 updater.start_polling(clean=True)
