5c3a9249b63c395deb4269773e9cce77ced06a71
YenotKuzya <75937913+YenotKuzya@users.noreply.github.com>
2021-01-15 23:58:17 +0500

    Create croco.py

---
+++ Bot/newbot/croco.py
@@ -0,0 +1,97 @@
+context.bot.send_message(chat_id=update.message.chat_id,
+                             text=qtn5)
+    timer = threading.Timer(15.0, gfg)
+    timer.start()
+    print('Cancelling timer\n')
+    timer.cancel()
+    print('Exit\n')
+    while True:
+        if update.message.text == 'повеситься':
+            context.bot.send_message(chat_id=update.message.chat_id,
+                                 text=ans1)
+        else:
+            context.bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+            time.sleep(2)
+            context.bot.send_message(chat_id=update.message.chat_id,
+                             text=ans)
+    time.sleep(2)
+
+
+
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text=qtn6)
+    timer = threading.Timer(15.0, gfg)
+    timer.start()
+    print('Cancelling timer\n')
+    timer.cancel()
+    print('Exit\n')
+    while True:
+        if update.message.text == 'кондуктор':
+            context.bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+            time.sleep(3)
+            context.bot.send_message(chat_id=update.message.chat_id,
+                                 text=ans1)
+        else:
+            context.bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+            time.sleep(2)
+            context.bot.send_message(chat_id=update.message.chat_id,
+                             text='Кондуктор')
+    time.sleep(2)
+    c
+
+
+
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text=qtn7)
+    timer = threading.Timer(15.0, gfg)
+    timer.start()
+    print('Cancelling timer\n')
+    timer.cancel()
+    print('Exit\n')
+    while True:
+        if update.message.text == 'таракан':
+            context.bot.send_message(chat_id=update.message.chat_id,
+                                 text='Сразу видно что ты услышал про своих')
+        else:
+            context.bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+            time.sleep(2)
+            context.bot.send_message(chat_id=update.message.chat_id,
+                             text='Таракан! Это был Таракан!')
+    time.sleep(2)
+
+
+
+
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text=qtn8)
+    timer = threading.Timer(15.0, gfg)
+    timer.start()
+    print('Cancelling timer\n')
+    timer.cancel()
+    print('Exit\n')
+    while True:
+        if update.message.text == 'спичку':
+            context.bot.send_message(chat_id=update.message.chat_id,
+                                 text=ans1)
+        else:
+            context.bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+            time.sleep(2)
+            context.bot.send_message(chat_id=update.message.chat_id,
+                             text='Эх, это была спичка. Ну ты даун:)')
+
+    context.bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+    time.sleep(2)
+    context.bot.send_message(chat_id=update.message.chat_id, text='Молодец, Рад был с тобой поиграть')
+    chat_id = update.message.chat_id
+    button_list = [
+        KeyboardButton(text='Продолжить игру', callback_data='clicked'),
+        KeyboardButton(text='Вернуться', callback_data=buttons_list)
+    ]
+    print(chat_id, 'clicked')
+
+    context.bot.send_message(
+        text='Choose button:',
+        chat_id=chat_id,
+        reply_markup=ReplyKeyboardMarkup([button_list
+                                          ])
+    )
